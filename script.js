97a0d60e126deb466627e517743663a0b1f5c265
Olena Reukova <kronverk01@gmail.com>
2023-02-03 21:39:17 +0000

    level 200
    Added Search

--- script.js
+++ script.js
@@ -8,26 +8,41 @@ function makeSeasonAndApisode(episode) {
   const { season, number } = episode;
   //const season = episode.season;
   //const number = episode.number;
-  const paddedSeason = season.toString().padStart(2,"0");
-const paddedEpisode = number.toString().padStart(2, "0");
+  const paddedSeason = season.toString().padStart(2, "0");
+  const paddedEpisode = number.toString().padStart(2, "0");
   return `S${paddedSeason}E${paddedEpisode}`;
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  const countParagraph = document.createElement("p");
+  countParagraph.innerText = `Showing ${episodeList.length} episodes`;
+  rootElem.appendChild(countParagraph);
+
   episodeList.forEach((episode) => {
     const paragraph = document.createElement("p");
     paragraph.textContent = ` ${makeSeasonAndApisode(episode)}`;
     rootElem.appendChild(paragraph);
-    const image = document.createElement('img');
+    const image = document.createElement("img");
     image.src = episode.image.medium;
     rootElem.appendChild(image);
 
-    const summaryParagraph = document.createElement("p");
-    summaryParagraph.innerHTML = episode.summary;
-    rootElem.appendChild(summaryParagraph);
+  
+    rootElem.innerHTML += episode.summary;
   });
 }
 
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", (event) => {
+  const searchString = event.target.value.toLowerCase();
+  const filteredEpisodes = getAllEpisodes().filter((episode) => {
+    return (
+      episode.summary.toLowerCase().includes(searchString) ||
+      episode.name.toLowerCase().includes(searchString)
+    );
+  });
+  makePageForEpisodes(filteredEpisodes);
+});
 window.onload = setup;
