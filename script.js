674db6542f25404d274e318c12c5850c46fa9036
Olena Reukova <kronverk01@gmail.com>
2023-02-20 04:53:55 +0000

    level 300

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //You can edit ALL of the code here
+
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -14,23 +17,30 @@ function makeSeasonAndApisode(episode) {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const selectElem = document.getElementById("select-input");
+  console.log(selectElem);
   rootElem.innerHTML = "";
   const countParagraph = document.createElement("p");
   countParagraph.innerText = `Showing ${episodeList.length} episodes`;
   rootElem.appendChild(countParagraph);
 
   episodeList.forEach((episode) => {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = ` ${makeSeasonAndApisode(episode)}`;
-    rootElem.appendChild(paragraph);
+    //add the season, apisode and name
+    const paragraph = document.createElement("p"); //create element
+    paragraph.textContent = ` ${makeSeasonAndApisode(episode)}`; //change it
+    rootElem.appendChild(paragraph); //append it
+    //add the image
     const image = document.createElement("img");
     image.src = episode.image.medium;
     rootElem.appendChild(image);
-
-  
+//add the summary paragraph nb the episode.summary is actually HTML
     rootElem.innerHTML += episode.summary;
+    //add it to the select element as an option (dropdown)
+    const option = document.createElement("option");
+    option.textContent = ` ${makeSeasonAndApisode(episode)} - ${episode.name}`;
+    option.value = episode.id;
+    selectElem.appendChild(option);
   });
 }
 
@@ -45,4 +55,20 @@ searchInput.addEventListener("input", (event) => {
   });
   makePageForEpisodes(filteredEpisodes);
 });
+
+
+//for select input
+const selectInput = document.getElementById("select-input");
+selectInput.addEventListener("change", (event) => {
+  //e.target.value ===the episode id that has been clicked on
+  console.log(event);
+  const idSelectedByUser = Number(event.target.value);
+  console.log({ idSelectedByUser });
+  const selectedEpisode = getAllEpisodes().find((episode) => episode.id === idSelectedByUser);
+  console.log(selectedEpisode);
+  if (selectedEpisode) {
+    makePageForEpisodes([selectedEpisode]);
+}
+});
+
 window.onload = setup;
