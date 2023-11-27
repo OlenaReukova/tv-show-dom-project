dc3b9cadf72dd8346d642ac84bfe17e3e7c805a5
Olena Reukova <kronverk01@gmail.com>
2023-11-27 22:58:52 +0000

    eslinton automatical editing

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-const url = "https://api.tvmaze.com/shows/82/episodes";
+const url = 'https://api.tvmaze.com/shows/82/episodes';
 
 let allEpisodes = [];
 //You can edit ALL of the code here
 
-const rootElem = document.getElementById("root");
+const rootElem = document.getElementById('root');
 
 function setup() {
-  fetch(url).then(res => res.json()).then(data => {
-    //here we do anything with data
-    console.log(data);
-    allEpisodes = data;
-    makePageForEpisodes(allEpisodes);
-  })
-  .catch ((err) => console.error(err));
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      //here we do anything with data
+      console.log(data);
+      allEpisodes = data;
+      makePageForEpisodes(allEpisodes);
+    })
+    .catch((err) => console.error(err));
 }
 
 function makeSeasonAndApisode(episode) {
   const { season, number, name } = episode;
   //const season = episode.season;
   //const number = episode.number;
-  const paddedSeason = season.toString().padStart(2, "0");
-  const paddedEpisode = number.toString().padStart(2, "0");
+  const paddedSeason = season.toString().padStart(2, '0');
+  const paddedEpisode = number.toString().padStart(2, '0');
   const episodeName = name;
   return `S${paddedSeason}E${paddedEpisode} ${episodeName}`;
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem =document.getElementById("root");
-  const selectElem = document.getElementById("select-input");
+  const rootElem = document.getElementById('root');
+  const selectElem = document.getElementById('select-input');
   console.log(selectElem);
   //clear out the rootElement's HTML before we add the new stuff
-  rootElem.innerHTML = "";
-  const countParagraph = document.createElement("p");
+  rootElem.innerHTML = '';
+  const countParagraph = document.createElement('p');
   countParagraph.innerText = `Showing ${episodeList.length} episodes`;
   rootElem.appendChild(countParagraph);
 
   episodeList.forEach((episode) => {
     //add the season, apisode and name
-    const paragraph = document.createElement("p"); //create element
+    const paragraph = document.createElement('p'); //create element
     paragraph.textContent = ` ${makeSeasonAndApisode(episode)}`; //change it
     rootElem.appendChild(paragraph); //append it
 
     //add the image
-    const image = document.createElement("img");
+    const image = document.createElement('img');
     image.src = episode.image.medium;
     rootElem.appendChild(image);
 
@@ -50,15 +52,15 @@ function makePageForEpisodes(episodeList) {
     rootElem.innerHTML += episode.summary;
 
     //add it to the select element as an option (dropdown)
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.textContent = ` ${makeSeasonAndApisode(episode)} - ${episode.name}`;
     option.value = episode.id;
     selectElem.appendChild(option);
   });
 }
 
-const searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", (event) => {
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', (event) => {
   const searchString = event.target.value.toLowerCase();
   const filteredEpisodes = allEpisodes.filter((episode) => {
     return (
@@ -70,8 +72,8 @@ searchInput.addEventListener("input", (event) => {
 });
 
 //for select input
-const selectInput = document.getElementById("select-input");
-selectInput.addEventListener("change", (event) => {
+const selectInput = document.getElementById('select-input');
+selectInput.addEventListener('change', (event) => {
   //e.target.value ===the episode id that has been clicked on
   console.log(event);
   const idSelectedByUser = Number(event.target.value);
